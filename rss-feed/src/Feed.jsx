eb99c28a3b5974f17e1b382db5d485b57f5ac034
aryan <aryan0312g@outlook.com>
2025-03-28 18:24:48 +0530

    adjusted the css for the feed for mobile screen width

--- rss-feed/src/Feed.jsx
+++ rss-feed/src/Feed.jsx
@@ -2,11 +2,11 @@ import React from "react";
 function Feed(props){
     console.log(props);
     return (
-        <>
+        <div>
         <h2>{props.title}</h2>
-        <p><a href={props.link} target="_blank" rel="noopener noreferrer">{props.link}</a></p>
+        <p style={{overflowWrap:"break-word"}}><a href={props.link} target="_blank" rel="noopener noreferrer">{props.link}</a></p>
         <h3>{props.pubDate}</h3>
-    </>
+    </div>
     )
 }
 export default Feed;
